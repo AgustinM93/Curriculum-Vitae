fd9754a31ea899bf2e9a071e40dadf4713a83253
AgustínM <agusstincho93@gmail.com>
2023-01-18 14:29:02 -0300

    agregando direccionamiento para Whats App y el Email

--- logica.js
+++ logica.js
@@ -1,4 +1,11 @@
-function direccionar(){
+function direccionarLI(){
     location.href = "https://www.linkedin.com/in/rodrigo-agustin-miguel/";
 }
 
+function direccionarWP(){
+    location.href = "https://api.whatsapp.com/send?phone=2920546244";
+}
+
+function direccionarMail(){
+    location.href = "mailto:agusstincho93@gmail.com";
+}
