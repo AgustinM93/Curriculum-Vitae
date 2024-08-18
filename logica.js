12566d2ced50c28336a7feee5e2f4b7d0c1f96cf
AgustínM <agusstincho93@gmail.com>
2024-08-18 01:09:14 -0300

    Fixing misdirection

--- logica.js
+++ logica.js
@@ -1,3 +1,11 @@
+function chooseLanguage(language) {
+    if (language === 'es') {
+        window.location.href = "curriculum-es.html"; // Redirige a la versión en español
+    } else if (language === 'en') {
+        window.location.href = "curriculum-en.html"; // Redirige a la versión en inglés
+    }
+}
+
 function direccionarLI(){
     location.href = "https://www.linkedin.com/in/rodrigo-agustin-miguel/";
 }
@@ -10,15 +18,3 @@ function direccionarMail(){
     location.href = "mailto:agusstincho93@gmail.com";
 }
 
-function chooseLanguage() {
-    var language = prompt("Elija su idioma / Choose your language: \n1. Español \n2. English");
-
-    if (language == "1") {
-        window.location.href = "curriculum-ES.html"; // Redirige a la versión en español
-    } else if (language == "2") {
-        window.location.href = "curriculum-EN.html"; // Redirige a la versión en inglés
-    } else {
-        alert("Opción no válida / Invalid option");
-        chooseLanguage(); // Llama a la función nuevamente si la opción es inválida
-    }
-}
